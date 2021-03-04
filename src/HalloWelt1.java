429b43b33a2da87bf60787619cefdae9e8998216
Cinda <cindarella0@hotmail.com>
2021-03-04 11:11:01 +0100

    Beistrich

---
+++ src/HalloWelt1.java
@@ -0,0 +1,23 @@
+// Kommentar
+/* mehrzeiliger Kommentag
+   1. Klassenname muss Dateiname entsprechen
+   Umbenennen = refactor
+ */
+public class HalloWelt1 {
+
+    // 3. psvm+Enter generiert die main-Methode
+    public static void main(String[] args) {
+        // Startpunkt für alle Programme
+
+        System.out.println("Hallo Welt am Tag 4321");
+        /*
+         Liebes System,
+         wir wollen etwas "out" geben
+         print
+         in Zeile-line "l" new "n"
+         übergeben mit " ...." einem Text
+         Abkürzung: sout+ENTER = System.out.println();
+        */
+        //TODO: Some more code
+    }
+}
