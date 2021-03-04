1fbf085cc1776892b81d2c2751d2c41552725f9f
Cinda <cindarella0@hotmail.com>
2021-03-04 11:25:50 +0100

    Unterschied Println u print

--- src/HalloWelt1.java
+++ src/HalloWelt1.java
@@ -2,8 +2,13 @@ public class HalloWelt1 {
     public static void main(String[] args) {
         System.out.println("Heute ist ein schöner Tag");
         System.out.println("neuer Tag?");
-        System.out.print("wann ist einer neuer Tag?");
+        System.out.println("wann ist einer neuer Tag?");
         System.out.print("morgen ist ein neuer Tag!");
+        System.out.print("morgen ist ein neuer Tag!");
+        System.out.print("morgen ist ein neuer Tag!");
+        System.out.println();
+        System.out.println();
+        System.out.println("morgen ist ein neuer Tag!");
         //TODO: Some more code
     }
 }
