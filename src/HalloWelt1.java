06518216784afb973e3e82e374fec2291e30219a
Cinda <cindarella0@hotmail.com>
2021-03-04 11:18:09 +0100

    mehr Zeilen

--- src/HalloWelt1.java
+++ src/HalloWelt1.java
@@ -1,6 +1,8 @@
 public class HalloWelt1 {
     public static void main(String[] args) {
         System.out.println("Heute ist ein schöner Tag");
+        System.out.println("neuer Tag?");
+        System.out.print("wann ist einer neuer Tag?")
         //TODO: Some more code
     }
 }
