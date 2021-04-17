e590b3be51455a42bb4f80bfc0483217010ee69e
Cinda <cindarella0@hotmail.com>
2021-04-17 19:13:36 +0200

    Wiederholung !

--- src/DistanceDemo.java
+++ src/DistanceDemo.java
@@ -1,15 +1,16 @@
 public class DistanceDemo {
     public static void main(String[] args) {
 
-        double kilometerValue;
-        System.out.println();
+        int kilometerValue;
+        kilometerValue = 10;
+        System.out.println(kilometerValue + "km sind " + kilometer(kilometerValue) + "meilen");
 
     }
 
     public static double kilometer(int kilometer){
         double milesPerKM;
-        milesPerKM = kilometer * 1.64;
+        milesPerKM = kilometer / 1.64;
 
-        return kilometer;
+        return milesPerKM;
     }
 }
